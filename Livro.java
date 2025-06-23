3263fd8be9972818ce79dcd8758b75f5d336431b
lazaro049 <lazaroaltermann4@gmail.com>
2025-06-22 21:22:08 -0300

    Enviando Classes

---
+++ Livro.java
@@ -0,0 +1,15 @@
+public class Livro {
+    String titulo;
+    boolean emprestado;
+
+    public void emprestar() {
+        emprestado = true;
+    }
+
+    public String status() {
+        if (emprestado) {
+            return "Indisponível";
+        }
+        return "Disponível";
+    }
+}
